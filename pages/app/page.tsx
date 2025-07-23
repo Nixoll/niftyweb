544fd64b13d798e01eaa8cef6521b93f4c8bd6bd
Nixoll <41959713+Nixoll@users.noreply.github.com>
2025-07-23 04:51:02 +0100

    Add files via upload

---
+++ pages/app/page.tsx
@@ -0,0 +1,28 @@
+
+'use client';
+
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import WhatWeDo from '@/components/WhatWeDo';
+import WhyNifty from '@/components/WhyNifty';
+import HowItWorks from '@/components/HowItWorks';
+import ProjectShowcase from '@/components/ProjectShowcase';
+import Founders from '@/components/Founders';
+import FinalCTA from '@/components/FinalCTA';
+import Footer from '@/components/Footer';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <Hero />
+      <WhatWeDo />
+      <WhyNifty />
+      <HowItWorks />
+      <ProjectShowcase />
+      <Founders />
+      <FinalCTA />
+      <Footer />
+    </div>
+  );
+}
